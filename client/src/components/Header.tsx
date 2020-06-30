e428e84effa46043d2a36495d3e146f8a346468a
Sammy Hajhamid <pokeso123@hotmail.com>
2020-06-30 11:51:51 -0700

    added cool backgrounds and header variants

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,13 +5,25 @@ import './Header.css';
 
 export default class Header extends React.Component<any, any> {
     render() {
-        return <motion.div className={'header'} animate={{
-            width: ['40vw', '80vw', '80vw'],
-            height: ['7vh', '7vh', '20vh'],
-        }} transition={{
-            duration: 3,
-            times: [.1, .35, .55],
-            ease: 'anticipate'
+        return <motion.div className={'header'} animate={'visible'} variants={{
+            visible: {
+                width: ['40vw', '80vw', '80vw'],
+                height: ['7vh', '7vh', '15vh'],
+                transition: {
+                    duration: 3,
+                    times: [.1, .35, .55],
+                    ease: 'anticipate',
+                    delay: 0.3,
+                    when: 'beforeChildren',
+                    staggerChildren: 0.1,
+                }
+            },
+            hidden: {
+                opacity: 0,
+                transition: {
+                    when: "afterChildren",
+                },
+            },
         }}>
             <motion.h2 className={'headerTitle'}>
                 Sammy Hajhamid
