6a39576b865833f4e427d64899d28588e2024534
Sammy Hajhamid <pokeso123@hotmail.com>
2020-07-01 16:40:19 -0700

    added badges and cool stuff

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,33 +1,66 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { motion } from 'framer-motion';
+import {motion} from 'framer-motion';
+import {Container, Row, Col} from 'react-grid-system';
 import './Header.css';
+
+const skills = ['ctf player', 'embedded & os', 'compiler & vm'];
 
 export default class Header extends React.Component<any, any> {
     render() {
         return <motion.div className={'header'} animate={'visible'} variants={{
             visible: {
-                width: ['40vw', '80vw', '80vw'],
-                height: ['7vh', '7vh', '15vh'],
+                width: ['10vw', '80vw', '80vw', '80vw'],
+                height: ['7vh', '7vh', '13vh', '20vh'],
+                y: [0, 0, 0, -100],
                 transition: {
-                    duration: 3,
-                    times: [.1, .35, .55],
+                    duration: 4,
+                    times: [.1, .25, .45, .1],
                     ease: 'anticipate',
                     delay: 0.3,
-                    when: 'beforeChildren',
-                    staggerChildren: 0.1,
                 }
-            },
-            hidden: {
-                opacity: 0,
-                transition: {
-                    when: "afterChildren",
-                },
-            },
+            }
         }}>
-            <motion.h2 className={'headerTitle'}>
-                Sammy Hajhamid
-            </motion.h2>
+            <Container>
+                <Row>
+                    <Col sm={7}>
+                        <motion.h2 className={'headerTitle'} variants={{
+                            hidden: {
+                                opacity: 0
+                            },
+                            show: {
+                                transition: {
+                                    delay: 2,
+                                },
+                                opacity: 1
+                            }
+                        }} initial={'hidden'} animate={'show'}>
+                            Sammy Hajhamid
+                        </motion.h2>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={5}>
+                        {skills.map((skill, i) => (
+                            <motion.span variants={{
+                                hidden: {
+                                    y: 70,
+                                    opacity: 0
+                                },
+                                visible: {
+                                    transition: {
+                                        delay: 3 + i * .4,
+                                    },
+                                    y: 0,
+                                    opacity: 1
+                                }
+                            }} initial={'hidden'} animate={'visible'} className={'skill'}>
+                                {skill}
+                            </motion.span>
+                        ))}
+                    </Col>
+                </Row>
+            </Container>
         </motion.div>
     }
 }
