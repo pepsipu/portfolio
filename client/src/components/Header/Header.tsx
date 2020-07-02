899ef4230af644026dd47cb4ac57ce4ab442e6c8
Sammy Hajhamid <pokeso123@hotmail.com>
2020-07-02 00:17:05 -0700

    restructured header and made the skills bounce and made them more adaptive than spans

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -2,20 +2,19 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {motion} from 'framer-motion';
 import {Container, Row, Col} from 'react-grid-system';
-import './Header.css';
 
-const skills = ['ctf player', 'embedded & os', 'compiler & vm'];
+import Skills from './Skills/Skills';
+import './Header.css';
 
 export default class Header extends React.Component<any, any> {
     render() {
         return <motion.div className={'header'} animate={'visible'} variants={{
             visible: {
-                width: ['10vw', '80vw', '80vw', '80vw'],
-                height: ['7vh', '7vh', '13vh', '20vh'],
-                y: [0, 0, 0, -100],
+                width: ['10vw', '80vw', '80vw'],
+                height: ['7vh', '7vh', '14vh'],
                 transition: {
                     duration: 4,
-                    times: [.1, .25, .45, .1],
+                    times: [.1, .25, .35],
                     ease: 'anticipate',
                     delay: 0.3,
                 }
@@ -41,23 +40,11 @@ export default class Header extends React.Component<any, any> {
                 </Row>
                 <Row>
                     <Col sm={5}>
-                        {skills.map((skill, i) => (
-                            <motion.span variants={{
-                                hidden: {
-                                    y: 70,
-                                    opacity: 0
-                                },
-                                visible: {
-                                    transition: {
-                                        delay: 3 + i * .4,
-                                    },
-                                    y: 0,
-                                    opacity: 1
-                                }
-                            }} initial={'hidden'} animate={'visible'} className={'skill'}>
-                                {skill}
-                            </motion.span>
-                        ))}
+                        <Row style={{
+                            margin: 0
+                        }}>
+                            <Skills/>
+                        </Row>
                     </Col>
                 </Row>
             </Container>
---
+++ client/src/components/Header/Skills/Skills.tsx
@@ -0,0 +1,26 @@
+import {motion} from 'framer-motion';
+import React from 'react';
+
+import './Skills.css';
+
+const skills = ['ctf player', 'embedded & os', 'compiler & vm'];
+
+const Skills = () => (
+    <>
+        {skills.map((skill, i) => (
+            <motion.div key={i} transition={{
+                delay: 3 + i * .2,
+            }} initial={{
+                opacity: 0,
+                y: 7
+            }} animate={{
+                opacity: 1,
+                y: 0
+            }} className={'skill'}>
+                {skill}
+            </motion.div>
+        ))}
+    </>
+);
+
+export default Skills;
