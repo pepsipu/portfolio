27a89f77946ed1ee0ce4213c41a01f309d2de770
Sammy Hajhamid <pokeso123@hotmail.com>
2020-07-07 18:56:52 -0700

    epically hacked my way into re adding hover/tap alongside the animations

--- client/src/components/Header/Nav/NavOptions/NavOptions.tsx
+++ client/src/components/Header/Nav/NavOptions/NavOptions.tsx
@@ -7,9 +7,11 @@ const navs = ['about_me', 'ctfs', 'projects'];
 
 const NavOptions = () => {
     return <motion.div className={'navOptions'} initial={{
-        width: '0%'
+        width: '0%',
+        opacity: 0
     }} animate={{
-        width: '100%'
+        width: '100%',
+        opacity: 100
     }} transition={{
         delay: 2,
         duration: 1
@@ -20,15 +22,24 @@ const NavOptions = () => {
             {navs.map((nav, i) => (
                 <Col key={`${i}navcol`} className={'navCol'}>
                     <motion.div key={`${i}nav`} transition={{
-                        delay: 3 + i * .3,
+                        delay: 3 + i * .25,
                     }} initial={{
                         opacity: 0,
                         y: -7
                     }} animate={{
                         opacity: 1,
                         y: 0,
-                    }} className={'navOption'}>
-                        {nav}
+                    }} style={{
+                        width: '100%',
+                        marginRight: '25px'
+                    }}>
+                        <motion.div whileHover={{
+                            scale: 1.05
+                        }} whileTap={{
+                            y: 2
+                        }} className={'navOption'}>
+                            {nav}
+                        </motion.div>
                     </motion.div>
                 </Col>
             ))}
